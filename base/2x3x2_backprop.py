dbfcebbba174d79f6a41bcb74cd515bb7f35f335
tardatio <tardatio@gmail.com>
2021-05-29 00:39:08 +0200

    backprop_improved

--- base/2x3x2_backprop.py
+++ base/2x3x2_backprop.py
@@ -19,10 +19,11 @@ def derivative(output,z,w2,x):
     part3 = np.ravel(1* z)
     w2_part = part1*part2
 
-
     # derivative w2
-    g2_ = w2_part*part3
-    g2 = derivative_transpose(g2_,w2)
+    mu3 = np.mean(part3)
+    g2_ = w2_part*mu3
+
+    g2 = derivative_transpose(g2_,len(w2))
 
     #derivative w1
     to_w1 = []
@@ -40,17 +41,22 @@ def derivative(output,z,w2,x):
 
 def derivative_transpose(data, w):
     x = np.array([data])
-    dt = np.vstack(list(x)*len(w)).T.ravel()
+    dt = np.vstack(list(x)*w).T.ravel()
     return dt
 
 def main():
 
-    x = np.array([0.05,0.10])
-    w1 = np.array([[0.15,0.20],[0.25,0.30]])
+    x = np.array([[0.05,0.10]])
+
+    # 2 * 3
+    w1 = np.array([[0.15,0.20,0.25],[0.20,0.25,0.40]])
     b1 = np.array([0.35])
-    w2 = np.array([[0.40, 0.45], [0.50, 0.55]])
-    b2 = np.array([0.60])
-    T = np.array([0.01,0.99])
+
+    # 3 * 2
+    w2 = np.array([[0.40, 0.45],[0.50, 0.35],[0.40,0.45]])
+    b2 = np.array([0.55])
+
+    T = np.array([0.34,0.66])
 
     epoch = 5000
     for i in range(epoch+1):
@@ -67,21 +73,22 @@ def main():
         # output layer
         wp2 = np.ravel(w2) #front
         #wb2 = np.flipud(w2).ravel() # back
-        wb2 = np.var(w2)
+        wb2 = np.var(wb2)
+
+        #wb2 = np.var(w2)
         outlayer = []
         for i in range(len(wp2)):
             outlayer.append(wp2[i] - wb2*g2[i])
 
         # hidden layer
         wp1 = np.ravel(w1) #front
-        bx = derivative_transpose(x, w1) # back
+        bx = derivative_transpose(x, len(w1[1])) # back
         hiddenlayer = []
         for i in range(len(wp1)):
             hiddenlayer.append(wp1[i] - bx[i]*g1[i])
 
-
-        w2 = np.array(outlayer).reshape(-1, len(w2))
-        w1 = np.array(hiddenlayer).reshape(-1, len(w1))
+        w2 = np.array(outlayer).reshape(len(w2),len(w2.T))
+        w1 = np.array(hiddenlayer).reshape(len(w1),len(w1.T))
 
 
 if __name__ == '__main__':
